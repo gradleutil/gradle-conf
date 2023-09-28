55e7714b4f299ea4e13c9c3d45707a347882b898
code@gradleutil.net <code@gradleutil.net>
2023-09-27 23:29:33 -0600

    add shadow summary

--- plugin/src/main/groovy/net/gradleutil/config/ConfProjectPlugin.groovy
+++ plugin/src/main/groovy/net/gradleutil/config/ConfProjectPlugin.groovy
@@ -10,12 +10,17 @@ import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.artifacts.DependencyResolutionListener
 import org.gradle.api.artifacts.ResolvableDependencies
+import org.gradle.api.internal.file.copy.DefaultFileCopyDetails
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.plugins.BasePlugin
 import org.gradle.api.plugins.JavaPluginConvention
 import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.api.publish.maven.tasks.AbstractPublishToMaven
+import org.gradle.api.publish.maven.tasks.PublishToMavenLocal
+import org.gradle.api.publish.maven.tasks.PublishToMavenRepository
 import org.gradle.api.tasks.SourceSet
 import org.gradle.api.tasks.TaskProvider
+import org.gradle.api.tasks.bundling.Jar
 import org.gradle.api.tasks.compile.JavaCompile
 import org.gradle.configuration.project.ProjectConfigurationActionContainer
 
@@ -127,6 +132,60 @@ class ConfProjectPlugin implements Plugin<Project> {
             }
         }
 
+        addMavenPublishInfo(project)
+        addShadowJarInfo(project)
+
+    }
+
+    static void addMavenPublishInfo(Project project){
+        project.tasks.withType(AbstractPublishToMaven).tap {
+            configureEach { AbstractPublishToMaven publishTask ->
+                publishTask.doLast {
+                    if (publishTask instanceof PublishToMavenRepository) {
+                        project.logger.lifecycle("Published ${project.group}.${project.name}:${project.version} to ${publishTask.repository.url}")
+                    } else if (publishTask instanceof PublishToMavenLocal) {
+                        String repoPath = project.repositories.mavenLocal().url.toURL().getFile()
+                        publishTask.publication.with { p ->
+                            def sb = new StringBuilder()
+                            p.artifacts.each {
+                                String artifactPath = p.groupId.replace(".", "/") + "/" + p.artifactId + "/" + p.version + "/" + p.artifactId + "-" + p.version
+                                sb.append('\n  ' + repoPath).append(artifactPath)
+                                        .append(it.classifier ? '-' + it.classifier : '').append('.' + it.extension)
+                            }
+                            project.logger.lifecycle("Published ${p.groupId}:${p.artifactId}:${p.version}${sb.toString()}")
+                        }
+                    } else {
+                        project.logger.lifecycle("Published ${project.group}:${project.name}:${project.version}")
+                    }
+                }
+            }
+        }
+    }
+
+    static String formatFileSize(String name, long length){
+        def formatStr = "%,10.2f"
+        return "${String.format(formatStr, length / 1024 / 1024)} Mb " + name
+    }
+
+    static void addShadowJarInfo(Project project) {
+        project.tasks.withType(Jar).configureEach { Jar jarTask ->
+            if(!jarTask.class.name.contains('shadow')){
+                return
+            }
+            Set<File> jars = []
+            jarTask.eachFile { DefaultFileCopyDetails file ->
+                if (file.name.endsWith('.jar')) {
+                    jars.add(file.file as File)
+                }
+            }
+            jarTask.doLast {
+                jars.sort { it.length() }.each { file ->
+                    jarTask.project.logger.lifecycle(formatFileSize(file.name, file.length()))
+                }
+                def jarFile = jarTask.outputs.files.first() as File
+                jarTask.project.logger.lifecycle('Total: '+ formatFileSize(jarFile.name, jarFile.length()))
+            }
+        }
     }
 
     private void configureJavaPluginDefaults() {
